ee696993a168a46d4da12b71e582fdb50725b4df
saloxiddin97 <salohiddinshamsidinov0101@gmail.com>
2024-06-28 19:49:14 +0500

    objekt

---
+++ script.js
@@ -0,0 +1,9 @@
+let data={name:"saloxiddin",id:884942,password:12475}
+let name=prompt('name kriting')
+let iduser=+prompt('ID kriting')
+let password=+prompt('PASSWORD parolni kriting')
+
+if(iduser===data.id && password===data.password&&name===data.name ){
+console.log(data);
+
+}
